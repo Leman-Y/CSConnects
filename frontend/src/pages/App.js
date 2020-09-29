b6421b052a9a6c055cbd71610df72a5d7d206001
Maisa Ahmad <2013maisaah@gmail.com>
2020-09-29 14:48:16 -0400

    created club card for home screen

---
+++ frontend/src/pages/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+import wics from '../images/wics.jpg';
+import acm from '../images/acm.png';
+import ClubCard from '../components/ClubCard';
+import ClubContainer from '../components/ClubContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        <ClubContainer>
+          <ClubCard image={wics}/>
+          <ClubCard image={acm}/>
+          <ClubCard image={acm}/>
+          <ClubCard image={acm}/>
+        </ClubContainer>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
