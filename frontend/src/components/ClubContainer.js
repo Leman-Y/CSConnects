69b766108c94a454b79dabcd3ff1422012b99c9d
Maisa Ahmad <2013maisaah@gmail.com>
2020-10-13 15:12:00 -0400

    added title

--- frontend/src/components/ClubContainer.js
+++ frontend/src/components/ClubContainer.js
@@ -3,9 +3,14 @@ import './ClubContainer.scss';
 
 const ClubContainer = (props) =>{
   return(
+    <div className="club-container">
+      <div className="club-title">
+        {props.title}
+      </div>
       <div className="wrapper">
         {props.children}
       </div>
+    </div>
   );
 }
 
