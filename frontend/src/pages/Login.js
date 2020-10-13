54098c5f4db70b9120276014f1ee7b1c8ca19f23
Maisa Ahmad <2013maisaah@gmail.com>
2020-10-13 13:10:54 -0400

    added sign up and login page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from '../components/Login';
+import Navigation from '../components/Navigation';
+
+function LoginPage() {
+  return (
+    <div>
+      <Navigation/>
+    <Login/>
+    </div>
+  );
+}
+
+export default LoginPage;
