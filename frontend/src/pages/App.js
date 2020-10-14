1953a440b4ad94a30b55ba93d93db07f38429fcf
Shaina Lowenthal <shaina.lowenthal96@myhunter.cuny.edu>
2020-10-13 21:18:18 -0400

    added twilio form to app

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -3,6 +3,7 @@ import {Switch, Route, Router} from '../util/router.js';
 import HomePage from './Home';
 import EventsPage from './Events';
 import SignUp from './SignUp';
+import SMSForm from '../SMSForm';
 
 function App(){
   return(
@@ -11,6 +12,7 @@ function App(){
         <Route exact path="/" component={HomePage}/>
         <Route exact path="/Events" component={EventsPage}/>
         <Route exact path="/SignUp" component={SignUp}/>
+        <Route exact path="/TwilioForm" component={SMSForm}/>
       </Switch>
     </Router>
 
