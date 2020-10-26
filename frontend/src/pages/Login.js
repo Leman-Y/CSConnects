d8321a45e010cca726ac140b39af21072d7d00a5
Maisa Ahmad <2013maisaah@gmail.com>
2020-10-25 20:56:04 -0400

    updated navigation for login/signup

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Login from '../components/Login';
 import Navigation from '../components/Navigation';
+import Computer from '../images/comp.svg';
+import burger from '../images/burger.svg';
 
 function LoginPage() {
   return (
     <div>
-      <Navigation/>
+      <div className="NavBar">
+        <Navigation icon={Computer} name="CSConnects" burger={burger}/>
+      </div>
     <Login/>
     </div>
   );
