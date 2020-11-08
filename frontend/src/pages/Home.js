f725ce83329608dfa1e0c1ecb12439a5442ed98f
Maisa Ahmad <2013maisaah@gmail.com>
2020-11-08 18:40:24 -0500

    added text and hover feature to clubcard

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -14,16 +14,10 @@ import IconContainer from '../components/IconContainer';
 import Computer from '../images/comp.svg';
 import burger from '../images/burger.svg';
 import '../styles/Home.scss';
-import {useRouter} from '../util/router.js'
-
-
 
 import Axios from 'axios';
 
-
-
 function HomePage() {
-  const router = useRouter();
 
   const [role, setRole] = useState('');
   Axios.defaults.withCredentials = true;
@@ -50,10 +44,10 @@ function HomePage() {
           <Icon icon={Celebrate} des="Have fun!"/>
         </IconContainer>
         <ClubContainer title="Meet our clubs!">
-          <ClubCard image={wics}/>
-          <ClubCard image={osc}/>
-          <ClubCard image={acm}/>
-          <ClubCard image={google}/>
+          <ClubCard image={wics} link="https://hunterwics.github.io/" id="1" des="Women in Computer Science is a club dedicated to all women interested in tech!"/>
+          <ClubCard image={osc} link="https://hunterosc.org/?fbclid=IwAR0C49eFyepwX__DJwg4nkggWhpE0d-cLl06DY1nu8f-8eltissn97QoRyo" id="2" des="Open Source Club is ....."/>
+          <ClubCard image={acm} link="https://www.hunteracm.org/" id="3" des="ACM is...."/>
+          <ClubCard image={google} link="https://dsc.community.dev/hunter-college/" id="4" des="Google Developer Student Club is..."/>
         </ClubContainer>
       </header>
     </div>
