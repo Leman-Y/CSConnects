84a0f22a179bcc8d57eb60613e1513802455e93a
Maisa Ahmad <2013maisaah@gmail.com>
2020-12-09 14:25:24 -0500

    fixed section for resources

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,23 +3,27 @@ import './Section.scss';
 
 const Section=(props)=>(
   <div className="section-container" >
-    <div className="section-top">
-      <div className="section-title">
-        {props.about_title}
+    <div className="about-page">
+      <div className="section-top">
+        <div className="section-title">
+          {props.about_title}
+        </div>
+        <div className="section-body">
+          {props.about}
+        </div>
+        <div className="section-body">
+          {props.about2}
+        </div>
       </div>
-      <div className="section-body">
-        {props.about}
-      </div>
-      <div className="section-body">
-        {props.about2}
-      </div>
-    </div>
-    <div className="section-bottom">
-      <div className="section-title">
-        {props.mission_title}
-      </div>
-      <div className="section-body">
-        {props.mission}
+      <div className="about-footer">
+        <div className="section-bottom">
+          <div className="section-title">
+            {props.mission_title}
+          </div>
+          <div className="section-body">
+            {props.mission}
+          </div>
+        </div>
       </div>
     </div>
   </div>
