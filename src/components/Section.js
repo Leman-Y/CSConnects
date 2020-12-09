3ca30f6b2a9bd9d33b1b35493f4439c3ed7412a7
Maisa Ahmad <2013maisaah@gmail.com>
2020-12-09 13:52:17 -0500

    removed image

--- src/components/Section.js
+++ src/components/Section.js
@@ -14,7 +14,6 @@ const Section=(props)=>(
         {props.about2}
       </div>
     </div>
-    <img alt="woman working on her computer" className="section-img" src = {props.img}/>
     <div className="section-bottom">
       <div className="section-title">
         {props.mission_title}
